9d38ccaf407e897eac510cbe54847789b600fe0a
ananduremanan <ananduor@gmail.com>
2023-07-19 17:34:28 +0530

    minor fixes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,7 +8,14 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let close = CustomMenuItem::new("close".to_string(), "Close");
+  let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+  let menu = Menu::new()
+    .add_submenu(submenu);
+
   tauri::Builder::default()
+    .menu(menu)
     .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
