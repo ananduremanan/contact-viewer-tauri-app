ab918433d3cb54c14c1e47cb46e1c5d84e764129
ananduremanan <ananduor@gmail.com>
2023-07-24 10:18:42 +0530

    Minor Changes

---
+++ src/App_Restructured.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import { invoke } from "@tauri-apps/api";
+import Layout from "./Components/Layout";
+
+export default function App() {
+  invoke("greet", { name: "World" }).then((response) => console.log(response));
+  return <Layout />;
+}
